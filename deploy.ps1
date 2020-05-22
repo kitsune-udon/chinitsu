7f678d18302c34a70daa69d5a6e11b83d8a86709
kitsune-udon <kitsune.udon.delicious@gmail.com>
2020-05-23 08:44:26 +0900

    fix deploy setting

--- deploy.ps1
+++ deploy.ps1
@@ -1,4 +1,7 @@
-test -e public && rm -rf public && mkdir public
+if (Test-Path public) {
+    rm -R public
+    mkdir public
+}
 elm make src/Main.elm --optimize --output public/main.js
 cp img public/
 cp index.html public/index.html
