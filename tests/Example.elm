cab56015d733dca446c69fa9aeda7ef40d378b67
kitsune-udon <kitsune.udon.delicious@gmail.com>
2020-05-23 08:45:56 +0900

    fix test

--- tests/Example.elm
+++ tests/Example.elm
@@ -2,9 +2,17 @@ module Example exposing (..)
 
 import Expect exposing (Expectation)
 import Fuzz exposing (Fuzzer, int, list, string)
+import Main exposing (machi)
 import Test exposing (..)
+
+
+tehai : List Int
+tehai =
+    [ 1, 1, 1, 2, 3, 4, 5, 6, 7, 8, 9, 9, 9 ]
 
 
 suite : Test
 suite =
-    describe "Always Pass Tests" [ test "Tort" <| \_ -> True |> Expect.equal True ]
+    describe "Always Pass Tests"
+        [ test "Churen" <| \_ -> machi tehai |> Expect.equal (List.range 1 9)
+        ]
