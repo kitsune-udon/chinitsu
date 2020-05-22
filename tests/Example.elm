3c4ced4cd0ccf8f393186cd1cf5eab99bfeb4e7a
kitsune-udon <kitsune.udon.delicious@gmail.com>
2020-05-22 19:52:34 +0900

    add test

---
+++ tests/Example.elm
@@ -0,0 +1,10 @@
+module Example exposing (..)
+
+import Expect exposing (Expectation)
+import Fuzz exposing (Fuzzer, int, list, string)
+import Test exposing (..)
+
+
+suite : Test
+suite =
+    describe "Always Pass Tests" [test "Tort" <| \_ -> True |> Expect.equal True]
